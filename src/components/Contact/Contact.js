dfe5dc3d69d99069bf19f126ff6a9b13b27f50e7
Jahid Hiron <namehiron.96@gmail.com>
2022-01-24 23:49:10 +0600

    initial commit

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,70 @@
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+import "./contact.css";
+import Phone from "../../img/phone.png";
+import Email from "../../img/email.png";
+import Address from "../../img/address.png";
+
+const Contact = () => {
+  const formRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_2ko4uxh",
+        "template_cgtxlz4",
+        formRef.current,
+        "user_tHuKF4B4WngyKKSdHEFTh"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  return (
+    <div className="c">
+      <div className="c-bg"></div>
+      <div className="c-wrapper">
+        <div className="c-left">
+          <h1 className="c-title">Let's discuss your project</h1>
+          <div className="c-info">
+            <div className="c-info-item">
+              <img src={Phone} alt="" className="c-icon" />
+              +1 1234 556 75
+            </div>
+            <div className="c-info-item">
+              <img className="c-icon" src={Email} alt="" />
+              contact@lama.dev
+            </div>
+            <div className="c-info-item">
+              <img className="c-icon" src={Address} alt="" />
+              245 King Street, Touterie Victoria 8520 Australia
+            </div>
+          </div>
+        </div>
+        <div className="c-right">
+          <p className="c-desc">
+            <b>What’s your story?</b> Get in touch. Always available for
+            freelancing if the right project comes along. me.
+          </p>
+          <form ref={formRef} onSubmit={handleSubmit}>
+            <input type="text" placeholder="Name" name="user_name" />
+            <input type="text" placeholder="Subject" name="user_subject" />
+            <input type="text" placeholder="Email" name="user_email" />
+            <textarea rows="5" placeholder="Message" name="message" />
+            <button>Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
